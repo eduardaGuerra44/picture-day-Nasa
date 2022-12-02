37346e0a5ae2f47b962e2d47e99653e2b81639f7
Eduarda Guerra <eduardaguerra44@gmail.com>
2022-12-01 21:13:46 -0300

    it´s possible to see the image and can read the description

--- api.js
+++ api.js
@@ -1,16 +1,24 @@
+// 
 let idButton = document.getElementById('button')
-
 idButton.addEventListener('click', () => {
-    console.log("oioioi")
+    console.log("botão clicado")
     respondeAPI()
 })
 
 
 
-async function respondeAPI () {
-let data = document.getElementById('data').value 
-let api = "60Pl3KMpg3Uhm6tZT9h9ccb7DothHXkuPHcV6huB"
-const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&date=${data}`)
-console.log(response)
+async function respondeAPI() {
+    let data = document.getElementById('data').value
+    let api = "60Pl3KMpg3Uhm6tZT9h9ccb7DothHXkuPHcV6huB"
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${api}&date=${data}`)
+    console.log(response)
+    let date = await response.json()
+    console.log(date)
+    usaApiDate(date)
+}
 
+
+function usaApiDate(date) {
+    document.querySelector('#descritivo').innerHTML += date.explanation
+    document.querySelector('#descritivo').innerHTML += `<img src="${date.url}">`
 }
